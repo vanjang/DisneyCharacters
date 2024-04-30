22a730e7a2d9fc40383eaf2d68a2bc19584f3206
Myung Hoon So <joa214@hotmail.com>
2024-04-30 18:54:42 +0100

    Data layer & Common

--- DisneyCharacters/Common/Extensions/Publisher+Extension.swift
+++ DisneyCharacters/Common/Extensions/Publisher+Extension.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import Combine
+
+// MARK: - FlatMapLatest
+extension Publisher {
+    func flatMapLatest<T: Publisher>(_ transform: @escaping (Self.Output) -> T) -> Publishers.SwitchToLatest<T, Publishers.Map<Self, T>> where T.Failure == Self.Failure {
+        map(transform).switchToLatest()
+    }
+    
+    static func empty() -> AnyPublisher<Output, Failure> {
+        return Empty().eraseToAnyPublisher()
+    }
+
+    static func just(_ output: Output) -> AnyPublisher<Output, Failure> {
+        return Just(output)
+            .setFailureType(to: Failure.self)
+            .eraseToAnyPublisher()
+    }
+
+    static func fail(_ error: Failure) -> AnyPublisher<Output, Failure> {
+        return Fail(error: error).eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Cache
+extension Publisher where Output == (data: Data, response: URLResponse), Failure == URLError {
+    func cache<T: Decodable>(using cache: URLCache, for request: URLRequest, with type: T.Type, decoder: JSONDecoder = JSONDecoder()) -> AnyPublisher<T, Error> {
+        return map { (data, response) in
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                let cachedResponse = CachedURLResponse(response: httpResponse, data: data)
+                cache.storeCachedResponse(cachedResponse, for: request)
+            }
+            return data
+        }
+        .decode(type: T.self, decoder: decoder)
+        .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Pagination
+extension Publisher where Failure == Never {
+    typealias PaginationItem = (limit: Int, offset: Int)
+    func paginate(limit: Int) -> AnyPublisher<PaginationItem, Never> {
+        scan(PaginationItem(limit: 0, offset: 0)) { last, _ in PaginationItem(limit: limit, offset: last.offset + limit) }
+            .eraseToAnyPublisher()
+    }
+}
--- DisneyCharacters/Data/ Repositories/MainPageRepository.swift
+++ DisneyCharacters/Data/ Repositories/MainPageRepository.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+final class MainPageRepository {
+    private let apiDataTransferService: DataTransferService
+    
+    init(apiDataTransferService: DataTransferService) {
+        self.apiDataTransferService = apiDataTransferService
+    }
+}
+
+extension MainPageRepository: MainPageRepositoryType {
+    func fetchCharacters(load: PassthroughSubject<Void, Never>, limit: Int) -> AnyPublisher<[Character], Error> {
+        load
+            .paginate(limit: limit)
+            .setFailureType(to: Error.self)
+            .map { paginationItem -> MainPageEndopoint in
+                let queryItems: [URLQueryItem] = [URLQueryItem(name: API.Queries.page.rawValue, value: "\(paginationItem.offset)"),
+                                                  URLQueryItem(name: API.Queries.pageSize.rawValue, value: "\(paginationItem.limit)")]
+                return MainPageEndopoint(path: API.Paths.character.rawValue, method: .get, parameters: queryItems)
+            }
+            .flatMapLatest { [unowned self] endpoint in
+                let characters: AnyPublisher<Response<[Character]>, Error> = self.apiDataTransferService.request(endpoint: endpoint)
+                return characters.map { $0.data }
+            }
+            .scan([Character](), { $0 + $1 })
+            .eraseToAnyPublisher()
+    }
+}
