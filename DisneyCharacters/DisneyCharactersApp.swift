6ee03bd12c0a1b2b6bf5993e51232af8a4b81d59
Myung Hoon So <joa214@hotmail.com>
2024-04-30 12:48:29 +0100

    initial commit

---
+++ DisneyCharacters/DisneyCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyCharactersApp.swift
+//  DisneyCharacters
+//
+//  Created by myung hoon on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DisneyCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
