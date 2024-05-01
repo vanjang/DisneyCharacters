fdf4966da6c3c1d6e2bec649be1dc29e1648795b
Myung Hoon So <joa214@hotmail.com>
2024-05-01 12:04:27 +0100

    Publisher extension

--- DisneyCharacters/Common/Extensions/Publisher+Extension.swift
+++ DisneyCharacters/Common/Extensions/Publisher+Extension.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-// MARK: - FlatMapLatest
+// MARK: - Helpers
 extension Publisher {
     func flatMapLatest<T: Publisher>(_ transform: @escaping (Self.Output) -> T) -> Publishers.SwitchToLatest<T, Publishers.Map<Self, T>> where T.Failure == Self.Failure {
         map(transform).switchToLatest()
@@ -50,5 +50,16 @@ extension Publisher where Failure == Never {
     func paginate(limit: Int) -> AnyPublisher<PaginationItem, Never> {
         scan(PaginationItem(limit: 0, offset: 0)) { last, _ in PaginationItem(limit: limit, offset: last.offset + limit) }
             .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Binder
+//https://forums.swift.org/t/does-assign-to-produce-memory-leaks/29546/9
+extension Publisher where Self.Failure == Never {
+    /// A method for assigning values to a key path of a given object, ensuring no strong reference cycle by holding a weak reference to the object.
+    public func assignNoRetain<Root>(to keyPath: ReferenceWritableKeyPath<Root, Self.Output>, on object: Root) -> AnyCancellable where Root: AnyObject {
+        sink { [weak object] (value) in
+            object?[keyPath: keyPath] = value
+        }
     }
 }
